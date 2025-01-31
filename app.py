9224fbcacf0ef42328c88196943190967c500559
Miriam Guendelman <miriam.guen@gmail.com>
2025-01-31 16:19:18 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import os
+
+# Set up the page
+st.set_page_config(page_title="EEG Viewer", layout="wide")
+st.title("EEG Example Viewer")
+
+# Define the folder containing EEG images
+image_folder = "figures"
+
+# Get a list of available EEG images
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+
+# Extract subjects and states from filenames
+subject_names = sorted(set(img.split("_")[0] for img in images))
+states = sorted(set(img.split("_")[1] for img in images))
+
+# Create dropdown menus for subject and state
+selected_subject = st.selectbox("Select a Subject:", subject_names)
+selected_state = st.selectbox("Select a State:", states)
+
+# Find matching image
+selected_image = f"{selected_subject}_{selected_state}_.png"
+if selected_image in images:
+    st.image(os.path.join(image_folder, selected_image), use_container_width=True)
+else:
+    st.warning("No image available for this selection.")
